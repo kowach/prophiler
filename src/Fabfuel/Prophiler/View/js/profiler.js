308caecf036c81ce65c6dec4c662e12892724bcd
Vladimir Kovačević <kowach@tvprofil.net>
2020-05-14 15:24:41 +0200

    Added option for injecting toolbar from ajax request from hidden html element

--- src/Fabfuel/Prophiler/View/js/profiler.js
+++ src/Fabfuel/Prophiler/View/js/profiler.js
@@ -16,8 +16,20 @@ $(document).on('click','#prophiler nav li', function() {
 
 // Only for development - toolbar - fetch ajax prophiler events
 $( document ).ajaxComplete(function(event, jqxhr, settings) {
+    // Prepend profiler_toolbar from AJAX JSON response
+    // { "profiler_toolbar" : "..." }
     if(typeof jqxhr.responseJSON !== 'undefined' && typeof jqxhr.responseJSON.profiler_toolbar !== 'undefined') {
-        console.log('Injecting profiler toolbar');
+        console.log('Injecting AJAX JSON profiler toolbar');
         $('#prophiler').prepend(jqxhr.responseJSON.profiler_toolbar);
+    }
+    // Prepend profiler_toolbar from AJAX HTML response from
+    //  &lt;script id="profiler_toolbar" type="text/x-custom-template"&gt;
+    //     ...
+    // &lt;/script&gt;
+    var profiler_toolbar=$('#profiler_toolbar');
+    if(profiler_toolbar.length>0) {
+        console.log('Injecting AJAX HTML profiler toolbar');
+        $('#prophiler').prepend(profiler_toolbar.html());
+        profiler_toolbar.remove();
     }
 });
